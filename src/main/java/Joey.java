7d14f40914929bb631789cf6135693e229bb28eb
Benjamin Wang <ben.wang9000@gmail.com>
2025-01-16 23:34:26 +0800

    Add chat history

--- src/main/java/Joey.java
+++ src/main/java/Joey.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Joey {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> history = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Joey!");
@@ -13,11 +15,25 @@ public class Joey {
         while (true) {
             System.out.print("You: ");
             String userInput = scanner.nextLine();
+
+            if (Objects.equals(userInput, "list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < history.size(); i++) {
+                    System.out.println((i + 1) + "." + history.get(i));
+                }
+                System.out.println("____________________________________________________________");
+
+                continue;
+            }
+
             if (Objects.equals(userInput, "bye")) {
                 break;
             }
+
+            history.add(userInput);
+
             System.out.println("____________________________________________________________");
-            System.out.println("Joey: " + userInput);
+            System.out.println("Joey: Added \"" + userInput + "\" to the history");
             System.out.println("____________________________________________________________");
         }
         System.out.println("____________________________________________________________");

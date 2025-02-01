79a743ec6fa23fff0b85e227b9c6e7c280eca3a9
Benjamin Wang <ben.wang9000@gmail.com>
2025-02-01 12:23:53 +0800

    Add dates and times

--- src/main/java/joey/task/Deadline.java
+++ src/main/java/joey/task/Deadline.java
@@ -2,31 +2,37 @@ package joey.task;
 
 import joey.enums.TaskType;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private String deadline;
+    private LocalDate by;
     private final TaskType type;
 
-    public Deadline(String description, String deadline) {
+    public Deadline(String description, LocalDate by) {
         super(description);
-        this.deadline = deadline;
+        this.by = by;
         this.type = TaskType.DEADLINE;
     }
 
     @Override
     public String toString() {
-        return "[" + this.type + "]" + super.toString() + " (by: " + this.deadline + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        String formattedDate = this.by.format(formatter);
+
+        return "[" + this.type + "]" + super.toString() + " (by: " + formattedDate + ")";
     }
 
     @Override
     public String getStorageFormat() {
-        return String.format("D|%s|%b|%s", getDescription(), isDone(), this.deadline);
+        return String.format("D|%s|%b|%s", getDescription(), isDone(), this.by);
     }
 
     public static Task createFromStorage(String data) {
         String[] parts = data.split("\\|");
         if (parts.length == 4) {
             System.out.println(parts[3]);
-            Deadline deadline = new Deadline(parts[1], parts[3]);
+            Deadline deadline = new Deadline(parts[1], LocalDate.parse(parts[3]));
             if (Boolean.parseBoolean(parts[2])) {
                 deadline.markDone();
             }
--- src/main/java/joey/task/Event.java
+++ src/main/java/joey/task/Event.java
@@ -2,12 +2,15 @@ package joey.task;
 
 import joey.enums.TaskType;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String startDate;
-    private String endDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
     private final TaskType type;
 
-    public Event(String description, String startDate, String endDate) {
+    public Event(String description, LocalDate startDate, LocalDate endDate) {
         super(description);
         this.startDate = startDate;
         this.endDate = endDate;
@@ -16,7 +19,12 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[" + this.type + "]" + super.toString() + " (from: " + this.startDate + " to: " + this.endDate + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        String formattedStartDate = this.startDate.format(formatter);
+        String formattedEndDate = this.endDate.format(formatter);
+
+        return "[" + this.type + "]" + super.toString() + " (from: "
+                + formattedStartDate + " to: " + formattedEndDate + ")";
     }
 
     @Override
@@ -27,7 +35,7 @@ public class Event extends Task {
     public static Task createFromStorage(String data) {
         String[] parts = data.split("\\|");
         if (parts.length == 5) {
-            Event event = new Event(parts[1], parts[3], parts[4]);
+            Event event = new Event(parts[1], LocalDate.parse(parts[3]), LocalDate.parse(parts[4]));
             if (Boolean.parseBoolean(parts[2])) {
                 event.markDone();
             }
